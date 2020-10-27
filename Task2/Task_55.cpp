433430fddc31c37535fcf7ffc40c45a15f2b6e6b
Tsimafei Smouzh <smoyzhh68@gmail.com>
2020-10-27 11:22:02 +0300

    Changed project structure, added task2

---
+++ Task2/Task_55.cpp
@@ -0,0 +1,14 @@
+﻿/*Copyright 2020 bu Tsimafei Smouzh*/
+#include <iostream>
+#include <cmath>
+
+int main() {
+double R = 10, m_R = 0, m_V = 1.6, V = 0;
+V = 4.0 / 3.0*3.14*pow(R, 3);
+std::cout << "V = " << V << "\n";
+m_R = pow(1.0 / 36.0 / 3.14*pow(1.0 / V, 2), 1.0 / 3.0)*m_V;
+std::cout << "mR = " << m_R << "\n";
+std::cout << "mR = " << m_R * 1000 << "\n";
+system("pause");
+return 0;
+}
